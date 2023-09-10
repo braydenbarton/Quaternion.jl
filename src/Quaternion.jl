443fc80c1cd56c957cf1d9d781327d3e538f007c
Brayden Barton <brayden.barton@usu.edu>
2023-09-10 13:30:01 -0600

    Add functions for matrix and axis-angle conversions

--- src/Quaternion.jl
+++ src/Quaternion.jl
@@ -1,5 +1,6 @@
 module Quaternion
-export ⊗, qconj, qvq, qnorm, qinv
+using LinearAlgebra
+export ⊗, qconj, qvq, qnorm, qinv, axis_angle_to_q, q_to_axis_angle, q2mat, axis_angle_to_mat, mat_to_axis_angle, mat2q
 
 """
     ⊗(p, q)
@@ -33,5 +34,50 @@ qnorm(q::Vector) = sqrt(q[1]^2 + q[2]^2 + q[3]^2 + q[4]^2)
 
 "Calculates the inverse of a quaternion in an efficient fashion"
 qinv(q::Vector) = qconj(q) / (q[1]^2 + q[2]^2 + q[3]^2 + q[4]^2)
+
+"Converts an axis of rotation and an angle of rotation (in radians) to a quaternion"
+axis_angle_to_q(vec::Vector, θ::Real) = [cos(θ/2); vec/norm(vec)*sin(θ/2)]
+
+"Recovers the axis-angle representation from a quaternion"
+q_to_axis_angle(q::Vector) = @views (q[2:4]/norm(q[2:4]), 2*atan(norm(q[2:4]), q[1]))
+
+"Converts a rotation quaternion to a rotation matrix"
+function q2mat(q::Vector)
+    s = qnorm(q) ^ -2
+    qr, qi, qj, qk = q
+    [
+        1 - 2s*(qj^2 + qk^2)    2s*(qi*qj - qk*qr)      2s*(qi*qk + qj*qr);
+        2s*(qi*qj + qk*qr)      1 - 2s*(qi^2 + qk^2)    2s*(qj*qk - qi*qr);
+        2s*(qi*qk - qj*qr)      2s*(qj*qk + qi*qr)      1 - 2s*(qi^2 + qj^2)
+    ]
+end
+
+"Converts an axis of rotation and an angle of rotation (in radians) to a rotation matrix"
+axis_angle_to_mat(vec::Vector, θ::Real) = q2mat(axis_angle_to_q(vec, θ))
+
+"Recovers the axis-angle representation from a rotation matrix"
+function mat_to_axis_angle(R::Matrix)
+    vec = nullspace(R-I)[:, 1]
+    vec /= norm(vec)
+    θ = acos((sum(diag(R)) - 1) / 2)
+    
+    # Find a vector orthogonal to vec
+    if abs(vec[1]) > abs(vec[2])
+        vec_a = [0, 1, 0]
+    else
+        vec_a = [1, 0, 0]
+    end
+    vec_b = cross(vec, vec_a)
+
+    # Resolve sign ambiguity in θ
+    if vec ⋅ cross(vec_b, R*vec_b) < 0
+        θ *= -1
+    end
+
+    return vec, θ
+end
+
+"Converts a rotation matrix to a quaternion"
+mat2q(R::Matrix) = axis_angle_to_q(mat_to_axis_angle(R)...)
 
 end
