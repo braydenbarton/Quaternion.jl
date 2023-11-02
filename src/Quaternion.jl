39276f358a037479c7bc9f96f3204846546691b2
Brayden Barton <brayden.barton@usu.edu>
2023-11-02 16:24:45 -0600

    Conert Quaternion to use StaticArrays

--- src/Quaternion.jl
+++ src/Quaternion.jl
@@ -1,48 +1,66 @@
 module Quaternion
-using LinearAlgebra
-export ⊗, qconj, qvq, qnorm, qinv, axis_angle_to_q, q_to_axis_angle, q2mat, axis_angle_to_mat, mat_to_axis_angle, mat2q
+using LinearAlgebra, StaticArrays
+export ⊗, qcomp, qconj, qvq, qnorm, qinv, axis_angle_to_q, q_to_axis_angle, q2mat, axis_angle_to_mat, mat_to_axis_angle, 
+    mat2q
+
+# Declaring convenience type aliases
+"Abstract quaternion representation"
+const Quat{T} = StaticVector{4, T} where T<:Real
+quat(q::AbstractVector) = SizedVector{4}(q)
+"Abstract representation for real 3-vecs (position, velocity, angular velocity, etc)"
+const RealVec{T} = StaticVector{3, T} where T<:Real
 
 """
     ⊗(p, q)
+    qcomp(p, q)
 
 Defines the quaternion composition operation for right-handed quaternions of the form [w; x; y; z] where `w` is 
 the scalar component and `xyz` are the vector components.
 """
-function ⊗(p::Vector, q::Vector)
+function ⊗(p::Quat, q::Quat)
     pw, px, py, pz = p
     qw, qx, qy, qz = q
-    [
+    SVector{4}([
         pw*qw - px*qx - py*qy - pz*qz,
         pw*qx + px*qw + py*qz - pz*qy,
         pw*qy - px*qz + py*qw + pz*qx,
         pw*qz + px*qy - py*qx + pz*qw
-    ]
+    ])
 end
+qcomp(p, q) = ⊗(p, q)
+"AbstractVector wrapping for old code"
+⊗(p::T1, q::T2) where {T1<:AbstractVector, T2<:AbstractVector} = (⊗(quat(p), quat(q)))
 
 """
     qconj(q)
 
 Calculates the conjugate of the given quaternion.
 """
-qconj(q::Vector) = [q[1]; -q[2:4]]
+qconj(q::Quat) = SVector{4}([q[1]; -q[2:4]])
+qconj(q::T) where T<:AbstractVector = qconj(quat(q))
 
 "Shorthand function for rotating a 3-element vector with the given quaternion"
-qvq(q::Vector, v::Vector) = (q⊗[0; v]⊗qconj(q))[2:4]
+qvq(q::Quat, v::RealVec) = SVector{3}((q ⊗ [0; v] ⊗ qconj(q))[2:4])
+qvq(q::T1, v::T2) where {T1<:AbstractVector, T2<:AbstractVector} = qvq(quat(q), SizedVector{3}(v))
 
 "Calculates the norm of a quaternion without using the LinearAlgebra package"
-qnorm(q::Vector) = sqrt(q[1]^2 + q[2]^2 + q[3]^2 + q[4]^2)
+qnorm(q::Quat) = sqrt(q[1]^2 + q[2]^2 + q[3]^2 + q[4]^2)
+qnorm(q::AbstractVector) = qnorm(quat(q))
 
 "Calculates the inverse of a quaternion in an efficient fashion"
-qinv(q::Vector) = qconj(q) / (q[1]^2 + q[2]^2 + q[3]^2 + q[4]^2)
+qinv(q::Quat) = qconj(q) / (q[1]^2 + q[2]^2 + q[3]^2 + q[4]^2)
+qinv(q::T) where T<:AbstractVector = qinv(quat(q))
 
 "Converts an axis of rotation and an angle of rotation (in radians) to a quaternion"
-axis_angle_to_q(vec::Vector, θ::Real) = [cos(θ/2); vec/norm(vec)*sin(θ/2)]
+axis_angle_to_q(vec::RealVec, θ::Real) = SVector([cos(θ/2); vec/norm(vec)*sin(θ/2)])
+axis_angle_to_q(vec::AbstractVector, θ::Real) = axis_angle_to_q(SizedVector{3}(vec), θ)
 
 "Recovers the axis-angle representation from a quaternion"
-q_to_axis_angle(q::Vector) = @views (q[2:4]/norm(q[2:4]), 2*atan(norm(q[2:4]), q[1]))
+q_to_axis_angle(q::RealVec) = @views (q[2:4]/norm(q[2:4]), 2*atan(norm(q[2:4]), q[1]))
+q_to_axis_angle(q::AbstractVector) = q_to_axis_angle(quat(q))
 
 "Converts a rotation quaternion to a rotation matrix"
-function q2mat(q::Vector)
+function q2mat(q::Quat)
     s = qnorm(q) ^ -2
     qw, qx, qy, qz = q
     [
@@ -51,9 +69,10 @@ function q2mat(q::Vector)
         2s*(qx*qz - qy*qw)      2s*(qy*qz + qx*qw)      1 - 2s*(qx^2 + qy^2)
     ]
 end
+q2mat(q::AbstractVector) = q2mat(quat(q))
 
 "Converts an axis of rotation and an angle of rotation (in radians) to a rotation matrix"
-axis_angle_to_mat(vec::Vector, θ::Real) = q2mat(axis_angle_to_q(vec, θ))
+axis_angle_to_mat(vec::RealVec, θ::Real) = q2mat(axis_angle_to_q(vec, θ))
 
 "Recovers the axis-angle representation from a rotation matrix"
 function mat_to_axis_angle(R::Matrix)
